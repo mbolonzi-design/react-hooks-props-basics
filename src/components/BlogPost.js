98eb1d23df42e6b062f880282edd9e6ac02a7b77
mbolonzi-design <mbolonzi.design@gmail.com>
2022-07-15 09:07:30 +0300

    commit tree e6100a0b6133700fdfbfa4b9248edc2f7d0a1d69

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,14 +1,20 @@
 import React from "react";
 import BlogContent from "./BlogContent";
 import Comment from "./Comment";
+import BlogRecomment from "./BlogRecomment";
 
 function BlogPost() {
   return (
     <div id="blog-post">
-      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration."
+      isPublished={true}
+      minutesToRead={1}
+       />
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+      <Comment commentText="I don't agree at all, in need to do my own research. - Mbolonzi" />
+      <BlogRecomment recommentSite="Visit here for more of this" />
     </div>
   );
 }
